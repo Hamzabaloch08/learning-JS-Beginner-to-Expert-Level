a0855e0d746494fbb7ca38cc9fb239b101ab18bb
Hamza Baloch <158695894+Hamzabaloch08@users.noreply.github.com>
2024-08-05 22:34:04 +0500

    learning of nested loop complete

--- app.js
+++ app.js
@@ -86,6 +86,12 @@
 //     }
 // }
 
+// for(let i = 1; i < 100; i=i+10){
+//     for(let j = i; j < i+10; j++ ){
+//         document.write(j , " ")
+//     }
+//     document.write("<br>")
+// }
 
 
 
