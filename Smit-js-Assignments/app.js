ee222b8701c37a7317c14c84eccd68777139f6e7
Hamza Baloch <158695894+Hamzabaloch08@users.noreply.github.com>
2024-08-17 22:41:46 +0500

    assignment no: 1

---
+++ Smit-js-Assignments/app.js
@@ -0,0 +1,6 @@
+const firstName = 'Hamza'
+const last_name = 'Baloch'
+let address = 'Main Street'
+address = 'First Street'
+console.log(`Name: ${firstName} ${last_name}`)
+console.log(`Address: ${address}`)
