eb778130018dfdd8b50f48165ef24dd5f9709d9a
Hamzabaloch08 <hb.developer08@gmail.com>
2024-10-01 06:12:07 +0500

    Update app.js

--- app.js
+++ app.js
@@ -189,33 +189,3 @@
 //     day: "numeric"
 // }))
 
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> concating array <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
-// const marval_heros = ['SpiderMan', 'IronMan', 'Thor','Hulk']
-// const dc_heros = ['Batman', 'Superman','Flash']
-
-// const allHeros = marval_heros.concat(dc_heros)
-// console.log(allHeros)
-
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> spread operator <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
-// const marval_heros = ['SpiderMan', 'IronMan', 'Thor','Hulk']
-// const dc_heros = ['Batman', 'Superman','Flash']
-//  const allHeros = [...marval_heros, ...dc_heros]
-//  console.log(allHeros)
-
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> falting array <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
-// let arr = [1, 2, 3, 4, 5, 6, [6, 7, 8, 8], 4, 23, 12, 53, [23, 3, 4, [3, 5, [3, 4], 1, 5]]]
-// let flatArray = arr.flat(Infinity)
-// console.log(flatArray)
-
-// console.log(Array.from("hamza"))
-
-// let score1 = 100;
-// let score2 = 200;
-// let score3 = 300;
-
-// let totalScore = Array.of(score1,score2,score3);
-// console.log(totalScore); // Output: [100, 200, 300]
-
