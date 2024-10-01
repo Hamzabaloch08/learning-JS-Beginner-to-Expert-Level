285d9ace54d476d641224fbe33279362d4b969a4
Hamzabaloch08 <hb.developer08@gmail.com>
2024-10-01 06:43:19 +0500

    learn functions and parameters

---
+++ functions-and-parameters/app.js
@@ -0,0 +1,31 @@
+// function sayMyName() {
+//   console.log("My name is John");
+// }
+// sayMyName();
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> adding number <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+// function addNumbers(num1, num2, num3, num4) {
+//     console.log(num1 + num2 + num3 + num4);
+// }
+// addNumbers(1, 2, 3, 4); // Output: 10
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> return  <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+// function addNumbers(num1, num2, num3, num4) {
+//   let sum = num1 + num2 + num3 + num4;
+//   return sum;
+// }
+// console.log(addNumbers(10,10,10,10)) // Output: 40
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> or <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+// function addNumbers(num1, num2, num3, num4) {
+//   return num1 + num2 + num3 + num4;
+// }
+// console.log(addNumbers(10, 10, 10, 10)); // Output: 40
+
+// function userLoginMessage(userName) {
+//     return `${userName} just loged In`
+// }
+// console.log(userLoginMessage('hb._07x'))
