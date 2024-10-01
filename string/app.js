344900a7543c849b582a7209b82ace3b9cda6117
Hamzabaloch08 <hb.developer08@gmail.com>
2024-10-01 06:17:54 +0500

    learn useful strings method

---
+++ string/app.js
@@ -0,0 +1,71 @@
+// method of strings
+
+// .toLowerCase()
+
+// let userInput = prompt("conver anything into lowercase")
+// document.write(userInput.toLowerCase())
+// The toLowerCase() method of String values returns this string converted to lower case.
+
+// .toUpperCase()
+
+// let userInput = prompt("conver anything into lowercase")
+// document.write(userInput.toUpperCase())
+// The toUpperCase() method of String values returns this string converted to uppercase.
+
+// let pro = prompt('Enter city name')
+// let pro1 = pro.slice(0,1).toUpperCase()
+// let pro2 = pro.slice(1).toLowerCase()
+
+// let res = pro1 + pro2
+// document.write(res)
+
+// .charAt()
+
+// let Name = "hamza"
+// document.write(Name.charAt("2"))
+
+// Math.round
+
+// let percentage = 72.4
+// document.write(Math.round(percentage))
+
+// Math.ceil
+
+// let percentage = 72.2
+// document.write(Math.ceil(percentage))
+
+// Math.floor
+
+// let percentage = 72.9
+// document.write(Math.floor(percentage))
+
+// Math.random
+
+// document.write(Math.random()*10)
+
+// function
+
+// function abc(){
+//     alert("hello world")
+// }
+
+// for(let i = 0; i < 10; i++){
+//     abc()
+// }
+
+// function sum(a, b, c) {
+//     return a + b + c
+// }
+
+// alert(sum(5, 6, 11));
+// alert(sum(11, 11, 11));
+
+// function sum(a = 2, b = 3, c = 4) {
+//     return a + b + c
+// }
+
+// alert(sum(10, 10, 10));
+// alert(sum());
+
+// let abc = 2.8888888888;
+// document.write(abc.toFixed(10))
