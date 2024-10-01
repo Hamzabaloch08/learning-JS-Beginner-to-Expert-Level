2becaa4bba62f59543a2a1aca317546bc15c0e58
Hamzabaloch08 <hb.developer08@gmail.com>
2024-10-01 06:12:03 +0500

    learn concatiing and flating or spreading arrays

--- array/app.js
+++ array/app.js
@@ -10,8 +10,6 @@
 // document.write("<br>")
 // document.write(array2)
 
-
-
 // .indexOf()
 
 // let userInput = prompt("Enter your name")
@@ -19,7 +17,6 @@
 // let res = studentNames.indexOf(userInput)
 
 // document.write(res)
-
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> .filter method
 
@@ -30,3 +27,33 @@
 // })
 
 // console.log(filteredData)
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> concating array <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+// const marval_heros = ['SpiderMan', 'IronMan', 'Thor','Hulk']
+// const dc_heros = ['Batman', 'Superman','Flash']
+
+// const allHeros = marval_heros.concat(dc_heros)
+// console.log(allHeros)
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> spread operator <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+// const marval_heros = ['SpiderMan', 'IronMan', 'Thor','Hulk']
+// const dc_heros = ['Batman', 'Superman','Flash']
+//  const allHeros = [...marval_heros, ...dc_heros]
+//  console.log(allHeros)
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> falting array <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+// let arr = [1, 2, 3, 4, 5, 6, [6, 7, 8, 8], 4, 23, 12, 53, [23, 3, 4, [3, 5, [3, 4], 1, 5]]]
+// let flatArray = arr.flat(Infinity)
+// console.log(flatArray)
+
+// console.log(Array.from("hamza"))
+
+// let score1 = 100;
+// let score2 = 200;
+// let score3 = 300;
+
+// let totalScore = Array.of(score1,score2,score3);
+// console.log(totalScore); // Output: [100, 200, 300]
