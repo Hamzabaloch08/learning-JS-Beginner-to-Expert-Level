ec2042b72a59cbc361c26533fafa7ceb96e0157d
Hamzabaloch08 <hb.developer08@gmail.com>
2024-10-01 06:06:00 +0500

    learn array

---
+++ array/app.js
@@ -0,0 +1,32 @@
+// let array = ["hamza","16",true,"ali"]
+// array.push("hsadgh")
+// array.pop()
+// array.unshift("web")
+// array.shift()
+// array.splice(1,1,"ali")
+// let array2 = array.slice(0)
+
+// document.write(array)
+// document.write("<br>")
+// document.write(array2)
+
+
+
+// .indexOf()
+
+// let userInput = prompt("Enter your name")
+// let studentNames = ["saad", "adeel", "ahmed", "ali"]
+// let res = studentNames.indexOf(userInput)
+
+// document.write(res)
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> .filter method
+
+// let data = [100,65,,64,76,567,578,345,123,45,432,654,754,567,754,234,908,890]
+
+// let filteredData = data.filter(function(data){
+//     return data > 500;
+// })
+
+// console.log(filteredData)
