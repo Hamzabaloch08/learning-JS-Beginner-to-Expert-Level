bfd357d1667db25e5bf30e0d5ca5ea6599cd6838
Hamzabaloch08 <hb.developer08@gmail.com>
2024-10-01 15:44:29 +0500

    learn how to control object in function

--- Functions-with-objects-&-array/app.js
+++ Functions-with-objects-&-array/app.js
@@ -1,14 +1,23 @@
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> rest operater <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> rest operater <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 // function calculateCartPrice(...price) {
 //   return price;
 // }
 // console.log(calculateCartPrice(100, 1000, 200, 5000));
 
-
 // function calculateCartPrice(value1, value2, ...price) {
 //   return price;
 // }
 // console.log(calculateCartPrice(100, 1000, 200, 5000));
 
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Object in function <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
+// let user = {
+//   name: "Hamza",
+//   age: 17,
+// };
+
+// function handelObject(object) {
+//   console.log(`username: ${object.name} age: ${user.age}`);
+// }
+// handelObject(user);
