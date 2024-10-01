3f6564af63bfd2286cc084dee31179595ebe083a
Hamzabaloch08 <hb.developer08@gmail.com>
2024-10-01 15:38:02 +0500

    learn rest operator

---
+++ Functions-with-objects-&-array/app.js
@@ -0,0 +1,14 @@
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> rest operater <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+// function calculateCartPrice(...price) {
+//   return price;
+// }
+// console.log(calculateCartPrice(100, 1000, 200, 5000));
+
+
+// function calculateCartPrice(value1, value2, ...price) {
+//   return price;
+// }
+// console.log(calculateCartPrice(100, 1000, 200, 5000));
+
+
