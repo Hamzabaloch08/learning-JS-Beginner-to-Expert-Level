541316e5caefeb0afb40da65aae2147548e558a3
Hamzabaloch08 <hb.developer08@gmail.com>
2024-10-01 06:14:36 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,4 @@
-// nested loop
 
-// let num = ["Front-End","Back-End","Full-stack",]
-// let num2 = ["Development","Developer"]
-
-// for (let i = 0; i < num.length; i++) {
-//     for (let j = 0; j < num2.length; j++) {
-//         document.write(num[i] , " " , num2[j],"<br>")
-//     }
-// }
-
-// for(let i = 1; i < 100; i=i+10){
-//     for(let j = i; j < i+10; j++ ){
-//         document.write(j , " ")
-//     }
-//     document.write("<br>")
-// }
 
 // method of strings
 
@@ -89,75 +73,7 @@
 // document.write(abc.toFixed(10))
 
 
-// >>>>>>>>>>>>>>>>>>>>>>>> loops <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-// >>>>>>>>>> for loop <<<<<<<<<<<<<<
-
-// for(let i = 0; i <= 10; i++){
-//     document.write(i , '<br>')
-// }
-
-// >>>>>>>>>>> while loop <<<<<<<<<<<<
-//  let number = 10
-//  while(number >= 1){
-//     document.write(number , '<br>')
-//     number--
-//  }
-
-//>>>>>>>>>>> Do While loop <<<<<<<<<<<<
-// let num = 1
-// do {
-//     document.write(num, '<br>')
-//     num++
-// } while (num <= 10)
-
-// function func1() {
-//     let obj = {
-//         name: "saad",
-//         age: 25,
-//     }
-
-//     func2(obj)
-// }
-
-// function func2(e){
-//     console.log(e)
-// }
-
-// func1()
-
-
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> loop forEach
-
-// let names = ['hamza','ahmed','raza','saad','rahul','ayan','hassan','usman','farhan','rehman']
-
-// names.forEach(function(printNames){
-//     document.write(`${printNames} <br>`)
-// })
-
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> forEach + .reverse() method
-
-// let names = ['hamza','ahmed','raza','saad','rahul','ayan','hassan','usman','farhan','rehman']
-// let reverseNames = names.reverse()
-// reverseNames.forEach(function(printReverseNames){
-//     document.write(`${printReverseNames} <br>`)
-// })
-
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> .map loop
-
-// let names = ['hamza','ahmed','raza','saad','rahul','ayan','hassan','usman','farhan','rehman']
-
-// names.map(function(printNames){
-//     document.write(`${printNames} `)
-// })
-
-// let fullName = 'jamsheed'
-// if (fullName == 'java' || 'html' || 'javascript') {
-//     console.log('you are a programmer')
-// }
-// else {
-//     console.log('you are not a programmer')
-// }
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> check some date method
 
