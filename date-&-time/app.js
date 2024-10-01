c26765fb0d74a7c2e18030d0c3bd4e161bdced5f
Hamzabaloch08 <hb.developer08@gmail.com>
2024-10-01 15:29:42 +0500

    learn about dete or time in javascript

---
+++ date-&-time/app.js
@@ -0,0 +1,30 @@
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> check some date method
+
+// let myDate = new Date();
+// document.write(myDate.toDateString())
+// document.write(myDate.toISOString())
+// document.write(myDate.toJSON())
+// document.write(myDate.toLocaleDateString())
+// document.write(myDate.toLocaleString())
+// document.write(myDate.toLocaleTimeString())
+
+// const myCreatedDate = new Date(2020, 0, 5)
+// console.log(myCreatedDate.toDateString())
+
+// let myTimeStamp = Date.now()
+// let myCreatedDate = new Date(2020, 0, 5)
+// console.log(myTimeStamp)
+// console.log(myCreatedDate.getTime())
+
+// console.log(Math.floor(Date.now()/1000))
+
+// let newDate = new Date();
+// console.log(newDate.getFullYear())
+// console.log(newDate.getMonth() + 1)
+
+// console.log(newDate.toLocaleString("default",{
+//     weekday: "long",
+//     year: "numeric",
+//     day: "numeric"
+// }))
+
