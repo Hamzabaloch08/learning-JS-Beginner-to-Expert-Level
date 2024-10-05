fe63aa645739c218e0b5ff6c198ac261c99296f3
Hamzabaloch08 <hb.developer08@gmail.com>
2024-10-05 18:44:40 +0500

    learn if and if else

---
+++ control_flow/app.js
@@ -0,0 +1,17 @@
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> if condition <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+const isUserLoggedIn = true
+if (isUserLoggedIn === true) {
+    console.log(`User was LoggedIn this Page`)
+}
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> else <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+const todayTemperature = 28
+if (todayTemperature < 30) {
+    console.log(`less then ${todayTemperature}`)
+} else {
+    console.log(`Greater then ${todayTemperature}`)
+}
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> else if <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
