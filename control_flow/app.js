2cc9796e126770b39fd23b874a5d6bd4be96c048
Hamzabaloch08 <hb.developer08@gmail.com>
2024-10-05 19:19:13 +0500

    learn if else if or else

--- control_flow/app.js
+++ control_flow/app.js
@@ -16,3 +16,16 @@ if (todayTemperature < 30) {
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> else if <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
+let balance = 500
+let userInput = +prompt(`enter your balance`)
+if (userInput === balance) {
+    // console.log(`your balance is ${userInput} you are eligible for this course`)
+} else if (userInput > balance) {
+    // console.log(`your balance is higher then ${balance} but its ok you are eligible for this course`)
+} else if (userInput < balance) {
+    // console.log(`your balance is ${userInput} you are not eligible for this course`)
+} else if (userInput === NaN) {
+    // console.log(`your balance is ${userInput} you are not eligible for this course`)
+} else {
+    // console.log(`please enter valid number or valid balance`)
+}
