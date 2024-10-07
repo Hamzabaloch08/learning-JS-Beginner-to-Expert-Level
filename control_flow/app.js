4d2e15a6edf61748b0b65c7094ac6ceec5e016f5
Hamzabaloch08 <hb.developer08@gmail.com>
2024-10-07 06:43:37 +0500

    learn if else or switch

--- control_flow/app.js
+++ control_flow/app.js
@@ -2,16 +2,16 @@
 
 // const isUserLoggedIn = true
 // if (isUserLoggedIn === true) {
-    // console.log(`User was LoggedIn this Page`)
+// console.log(`User was LoggedIn this Page`)
 // }
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> else <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 // const todayTemperature = 28
 // if (todayTemperature < 30) {
-    // console.log(`less then ${todayTemperature}`)
+// console.log(`less then ${todayTemperature}`)
 // } else {
-    // console.log(`Greater then ${todayTemperature}`)
+// console.log(`Greater then ${todayTemperature}`)
 // }
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> else if <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
@@ -19,13 +19,70 @@
 // let balance = 500
 // let userInput = +prompt(`enter your balance`)
 // if (userInput === balance) {
-    // console.log(`your balance is ${userInput} you are eligible for this course`)
+// console.log(`your balance is ${userInput} you are eligible for this course`)
 // } else if (userInput > balance) {
-    // console.log(`your balance is higher then ${balance} but its ok you are eligible for this course`)
+// console.log(`your balance is higher then ${balance} but its ok you are eligible for this course`)
 // } else if (userInput < balance) {
-    // console.log(`your balance is ${userInput} you are not eligible for this course`)
+// console.log(`your balance is ${userInput} you are not eligible for this course`)
 // } else if (userInput === NaN) {
-    // console.log(`your balance is ${userInput} you are not eligible for this course`)
+// console.log(`your balance is ${userInput} you are not eligible for this course`)
 // } else {
-    // console.log(`please enter valid number or valid balance`)
+// console.log(`please enter valid number or valid balance`)
+// }
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> switch case <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+// let month = Math.floor(Math.random() * 12) + 1;
+// console.log(month)
+
+// switch (month) {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("February");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+//     case 5:
+//         console.log("May");
+//         break;
+//     case 6:
+//         console.log("June");
+//         break;
+//     case 7:
+//         console.log("July");
+//         break;
+//     case 8:
+//         console.log("August");
+//         break;
+//     case 9:
+//         console.log("September");
+//         break;
+//     case 10:
+//         console.log("October");
+//         break;
+//     case 11:
+//         console.log("November");
+//         break;
+//     case 12:
+//         console.log("December");
+//         break;
+//     default:
+//         console.log("Invalid month");
+//         break;
 // }
