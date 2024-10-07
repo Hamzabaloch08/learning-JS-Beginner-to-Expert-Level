a623ced676135a87291dc6566ded790c4d8b6220
Hamzabaloch08 <hb.developer08@gmail.com>
2024-10-08 02:55:36 +0500

    learn for loop

---
+++ iterations/app.js
@@ -0,0 +1,19 @@
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> for loop <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+
+// } => syntex of forloop
+
+
+// inward counting
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// backward counting
+
+// for(let i = 10; i >= 0; i--){
+//     console.log(i);
+// }
