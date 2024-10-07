192411631944f64dbe67d5d530d2b40a15f0de7e
Hamzabaloch08 <hb.developer08@gmail.com>
2024-10-08 03:20:49 +0500

    learn if in for loop nested loop

--- iterations/app.js
+++ iterations/app.js
@@ -17,3 +17,38 @@
 // for(let i = 10; i >= 0; i--){
 //     console.log(i);
 // }
+
+// printing table
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`2 x ${i} = ${i * 2}`)
+// }
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> if in forloop <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+// for(let i = 0; i <= 10; i++){
+//     const numbers = i
+//     if (numbers == 5){
+//         console.log(`5 is best number!`)
+//     }
+//     console.log(numbers);
+
+// }
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> nested loop <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+// for (let i = 0; i <=10; i++){
+//     console.log(`outer loop ${i}`)
+//     for (let j = 0; j <= 10; j++){
+//         console.log(`inner loop ${j} outer loop ${i}`);
+//         }
+// }
+
+// table printing
+
+for (let i = 2; i <= 10; i++) {
+    document.write(`<br>Table of ${i} <br><br>`)
+    for (let j = 1; j <= 10; j++) {
+        document.write(`${i} x ${j} = ${i * j} <br>`);
+    }
+}
