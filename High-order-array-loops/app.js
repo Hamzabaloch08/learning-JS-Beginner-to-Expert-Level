ccbf76dc7dddd0f1e61761a7c6e873b32cf3004b
Hamzabaloch08 <hb.developer08@gmail.com>
2024-10-09 02:55:53 +0500

    learn for of or datatype map

---
+++ High-order-array-loops/app.js
@@ -0,0 +1,20 @@
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> for of <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+// const arr = [1, 2, 3, 4, 5]
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+// const greetings = "Hello world!"
+// for (const greet of greetings) {
+//     console.log(`Each char is ${greet}`)
+// }
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> dataType Maps <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+const map = new Map()
+map.set('name', 'john');
+map.set('age', '18');
+map.set('city', 'NYC')
+
+console.log(map)
