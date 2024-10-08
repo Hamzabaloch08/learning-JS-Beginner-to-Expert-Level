33ab9a758c5b94626682991e6adc113eb7a84961
Hamzabaloch08 <hb.developer08@gmail.com>
2024-10-09 02:43:43 +0500

    learn do while loop

--- iterations/app.js
+++ iterations/app.js
@@ -95,3 +95,11 @@
 //     console.log(arr[index])
 //     index++
 // }
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> do while loop <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+// let score = 1;
+// do {
+//     console.log(`Your score is ${score}`)
+//     score++
+// } while (score <= 10)
