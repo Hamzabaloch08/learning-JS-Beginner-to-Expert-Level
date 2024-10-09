9d88aa90ce4d5d52d6abc9c8a039643aa6f03f3c
Hamzabaloch08 <hb.developer08@gmail.com>
2024-10-10 02:42:33 +0500

    forEach does not return anything

--- Filter-map-and-reduce-in-javascript/app.js
+++ Filter-map-and-reduce-in-javascript/app.js
@@ -1,7 +1,7 @@
 const coding = ['js', 'py', 'java', 'c++', 'c#', 'ruby', 'cbb']
 
 let result = coding.forEach((item) => {
-    return item;
+    return item;   // >>>>>>>>>>>>>>>>>>>>>>>>>>> forEach does not return anything
 })
 
 console.log(result) //undefined
