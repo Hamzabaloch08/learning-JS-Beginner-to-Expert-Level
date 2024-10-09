fb37f67fec65d4c1f1f4b350af03bccac852aff6
Hamzabaloch08 <hb.developer08@gmail.com>
2024-10-10 03:17:28 +0500

    learn filter operation or map

--- Filter-map-and-reduce-in-javascript/app.js
+++ Filter-map-and-reduce-in-javascript/app.js
@@ -1,7 +1,56 @@
 const coding = ['js', 'py', 'java', 'c++', 'c#', 'ruby', 'cbb']
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 let result = coding.forEach((item) => {
-    return item;   // >>>>>>>>>>>>>>>>>>>>>>>>>>> forEach does not return anything
+    return item;   // => forEach does not return anything
+})
+// console.log(result); //undefined
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> method filter operation <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+let filteredValues = num.filter((num) => {
+    return num > 4
+})
+// console.log(filteredValues);
+
+const books = [
+    {
+        title: 'Book 1', genre: 'History', publish: '1990'
+    },
+    {
+        title: 'Book 2', genre: 'Fiction', publish: '1995'
+    },
+    {
+        title: 'Book 3', genre: 'History', publish: '1998'
+    },
+    {
+        title: 'Book 4', genre: 'Science', publish: '1999'
+    },
+    {
+        title: 'Book 5', genre: 'Fiction', publish: '1997'
+    },
+    {
+        title: 'Book 6', genre: 'History', publish: '2005'
+    },
+    {
+        title: 'Book 7', genre: 'Non-Fiction', publish: '2000'
+    },
+    {
+        title: 'Book 8', genre: 'History', publish: '1994'
+    }
+]
+
+let userBooks = books.filter((seletedBooks) => {
+    return seletedBooks.publish >= 2000 && seletedBooks.genre === 'History'
+})
+// console.log(userBooks);
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> .map <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+let newNums = num.map((num) => {
+    return num + 10 // => map returns a new array with the results of the function applied to each element,
 })
 
-console.log(result) //undefined
+console.log(newNums);   
