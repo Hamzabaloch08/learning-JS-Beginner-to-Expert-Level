2932dbfd09e72381394178c9639687e63c8ae151
Hamzabaloch08 <hb.developer08@gmail.com>
2024-10-17 03:42:14 +0500

    learn event in javaScript

--- Events-in-Javascript/app.js
+++ Events-in-Javascript/app.js
@@ -1,3 +1,28 @@
-document.querySelector('#owl').addEventListener('click', (e) => {
-    console.log(e)
-}, false)
+// document.querySelector('#owl').addEventListener('click', (e) => {
+//     console.log(e)
+// }, false)
+
+
+// document.querySelector('#images').addEventListener('click', (e) => {
+//     console.log("click inside the ul");
+// }, false)
+
+
+// document.querySelector('#owl').addEventListener('click', (e) => {
+//     console.log("owl clicked");
+//     e.stopPropagation();
+// }, false)
+
+
+// document.querySelector('#google').addEventListener('click', (e) => {
+//     e.preventDefault()
+//     e.stopPropagation()
+//     console.log("google clicked");
+// }, false)
+
+
+document.querySelector('#images').addEventListener('click', (e) => {
+    if(e.target.tagName === 'IMG'){
+        e.target.parentNode.remove()
+    }
+})
