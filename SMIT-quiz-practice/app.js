d0fe5c5faa257434cab6aa852b3b7b31b5611b52
Hamzabaloch08 <hb.developer08@gmail.com>
2024-10-19 11:39:10 +0500

    quiz practice for quiz test at smit

---
+++ SMIT-quiz-practice/app.js
@@ -0,0 +1,52 @@
+// for (let i = 1; i <= 100; i = i + 10) {
+//     for (let j = i; j < i + 10; j++) {
+//         document.write(`${j} `);
+//     }
+//     document.write(`<br>`)
+// }
+
+// 20. For Loops (Nested)
+// Q1: What will be the output of the following code?
+
+
+// for (let i = 0; i < 2; i++) {
+//     for (let j = 0; j < 2; j++) {
+//         console.log(i, j);
+//     }
+// }
+
+// for (let i = 0; i < 2; i++) {
+//     console.log('i=>', i)
+// }
+
+// for (let j = 0; j < 2; j++) {
+//     console.log('j=>',j);
+// }
+
+
+// Q2: How many times will the inner loop execute if both the outer and inner loops are as follows?
+
+// for (let i = 0; i < 4; i++) {
+//     for (let j = 0; j < 2; j++) {
+//         console.log(i, j)
+//     }
+// }
+
+
+// 21. Changing Case
+
+// Q3: Which method is used to convert a string to lowercase in JavaScript?
+
+// A) toLower()
+// B) capitalize()
+// C) toLowerCase()
+// D) toCasefold()
+
+// Q4: What will be the output of the following code?
+
+// console.log("Hello".toUpperCase());
+
+// A) hello
+// B) HELLO
+// C) Hello
+// D) hello world
