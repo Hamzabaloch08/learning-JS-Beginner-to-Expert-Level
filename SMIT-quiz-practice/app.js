b10ca41413a498019397f617bf2a95f31da52a05
Hamzabaloch08 <hb.developer08@gmail.com>
2024-10-19 13:22:54 +0500

    Update app.js

--- SMIT-quiz-practice/app.js
+++ SMIT-quiz-practice/app.js
@@ -33,7 +33,9 @@
 // }
 
 
+//chapter
 // 21. Changing Case
+
 
 // Q3: Which method is used to convert a string to lowercase in JavaScript?
 
@@ -71,6 +73,7 @@
 // D) Java
 
 
+//chapter
 // 23. Strings: Finding Segments
 
 
@@ -93,6 +96,7 @@
 // D) 12
 
 
+//chapter
 // 24. Strings: Finding a Character at a Location
 
 
@@ -106,10 +110,11 @@
 
 // Q10: What will the following code output?
 
-let str = "Hello World";
-console.log(str.charAt(6));
+// let str = "Hello World";
+// console.log(str.charAt(6));
 
 
+//chapter
 // 25. Strings: Replacing Characters
 
 // Q11: Which method is used to replace all occurrences of a substring in a string in JavaScript?
@@ -133,6 +138,7 @@ console.log(str.charAt(6));
 // console.log(sentence.replaceAll("apples", "oranges"));
 
 
+//chapter
 // 26. Rounding Numbers
 
 
@@ -151,7 +157,9 @@ console.log(str.charAt(6));
 // D) 7
 
 
+//chapter
 // 27. Generating Random Numbers
+
 
 // Q15: Which method is used to generate a random number between 0 and 1 in JavaScript?
 
@@ -168,7 +176,9 @@ console.log(str.charAt(6));
 // D) Math.ceil(Math.random() * 10)
 
 
+//chapter
 // 28. Converting Strings to Integers and Decimals
+
 
 // Q17: Which function is used to convert a string to an integer in JavaScript?
 
@@ -185,7 +195,9 @@ console.log(str.charAt(6));
 // D) NaN 
 
 
+//chapter
 // 29. Converting Strings to Numbers and Numbers to Strings
+
 
 // Q19: How do you convert the number 100 into a string in JavaScript?
 
@@ -193,3 +205,111 @@ console.log(str.charAt(6));
 // B) String(100)
 // C) 100.convertToString()
 // D) Both A and B => correct
+
+// Q20: What is the output of the following code?
+
+// let num = 100;
+// console.log(num.toString());
+
+// A) 100 (as a string) => correct
+// B) 100 (as a number)
+// C) Error
+// D) 1
+
+
+//chapter
+// 30. Controlling the Length of Decimals
+
+
+// Q21: Which method is used to format a number to a fixed number of decimal places?
+
+// A) toFixed() => correct
+// B) toPrecision()
+// C) Math.round()
+// D) truncate()
+
+// Q22: What will the following code output?
+
+// let num = 5.6789;
+// console.log(num.toFixed(2));  // Output: 5.68
+
+// A) 5.67
+// B) 5.678
+// C) 5.68
+// D) 6.00
+
+
+//chapter
+// 31. Getting the Current Date and Time
+
+
+// Q23: Which method is used to get the current date and time in JavaScript?
+
+// A) new Date() => correct 
+// B) Date.now()
+// C) getCurrentDate()
+// D) currentDate()
+
+// Q24: What is the output of Date.now()?
+
+// A) Current date in string format
+// B) Unix timestamp (milliseconds since 1970) => correct
+// C) Current time in 24-hour format
+// D) Timezone offset
+
+
+//chapter
+// 32. Extracting Parts of the Date and Time
+
+
+// Q25: How can you get the current year using the Date object in JavaScript?
+
+// A) Date.getFullYear() => correct
+// B) new Date().getYear()
+// C) new Date().getFullYear()
+// D) Date().getYear()
+
+
+// Q26: What will the following code output?
+
+let today = new Date();
+console.log(today.getMonth());
+
+// A) Current month (0-11) => correct
+// B) Current month (1-12)
+// C) Month name
+// D) NaN
+
+
+//chapter
+// Functions
+
+// What is the correct syntax for defining a function in JavaScript?
+
+// A) function myFunction: { }
+// B) function myFunction() { }
+// C) myFunction() = function { }
+// D) function: myFunction() { }
+
+// Answer: B) function myFunction() { }
+
+
+// Which of the following statements correctly calls a function named calculateSum?
+
+// A) calculateSum[]
+// B) calculateSum{}
+// C) calculateSum()
+// D) call calculateSum()
+
+// Answer: C) calculateSum()
+
+
+// Passing Data to Functions
+// How do you pass arguments to a function in JavaScript?
+
+// A) By enclosing them in parentheses.
+// B) By using curly braces.
+// C) By separating them with commas within the parentheses.
+// D) By placing them in square brackets.
+
+// Answer: C) By separating them with commas within the parentheses.
