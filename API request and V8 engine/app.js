1681cc4f463f68109a897c0cf416fcd5b2049a54
Hamzabaloch08 <hb.developer08@gmail.com>
2024-10-25 16:29:48 +0500

    Update app.js

--- API request and V8 engine/app.js
+++ API request and V8 engine/app.js
@@ -1,1 +1,11 @@
-const xhr = new XMLHttpRequest()
+const requestUrl = 'https://api.github.com/users/Hamzabaloch08'
+const xhr = new XMLHttpRequest();
+xhr.open('GET', requestUrl);
+xhr.onreadystatechange = () => {
+    console.log(xhr.readyState);
+    if (xhr.readyState === 4) {
+        const data = JSON.parse(xhr.responseText);
+        console.log(`${data.followers} => userFollwers`);
+    }
+}
+xhr.send()
