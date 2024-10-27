8f61c65fe75816e35704c4fb3c461cdb582cb641
Hamzabaloch08 <hb.developer08@gmail.com>
2024-10-28 02:49:19 +0500

    Update app.js

--- Promises/app.js
+++ Promises/app.js
@@ -1,54 +1,29 @@
-const promiseOne = new Promise((resolve, reject) => {
-    // do an async task 
-    // DB calls, cryptography, network
-    setTimeout(() => {
-        console.log('Async task complete')
-        resolve()
-    }, 1000)
-});
+// const promiseOne = new Promise((resolve, reject) => {
+//     // do an async task 
+//     // DB calls, cryptography, network
+//     setTimeout(() => {
+//         console.log('Async task complete')
+//         resolve()
+//     }, 1000)
+// });
 
-promiseOne.then(() => {
-    console.log('Promise consume')
-});
-
-
-
-
-
-
-new Promise((resolve, reject) => {
-    setTimeout(() => {
-        console.log('Async task 2')
-        resolve()
-    }, 1000)
-}).then(() => {
-    console.log('Promise consume 2')
-})
+// promiseOne.then(() => {
+//     console.log('Promise consume')
+// });
 
 
 
 
 
 
-
-
-
-const promiseThree = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve({
-            userName: 'hb07',
-            password: '12345678900',
-            emailAddress: 'example@gmail.com',
-            userAge: 17,
-            followers: 659,
-            following: 233
-        });
-    }, 1000)
-})
-
-promiseThree.then((userData) => {
-    console.log(`userName => ${userData.userName}\nuserpassword is => ${userData.password}`)
-})
+// new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         console.log('Async task 2')
+//         resolve()
+//     }, 1000)
+// }).then(() => {
+//     console.log('Promise consume 2')
+// })
 
 
 
@@ -57,27 +32,88 @@ promiseThree.then((userData) => {
 
 
 
-const promiseFour = new Promise((resolve, reject) => {
+
+// const promiseThree = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve({
+//             userName: 'hb07',
+//             password: '12345678900',
+//             emailAddress: 'example@gmail.com',
+//             userAge: 17,
+//             followers: 659,
+//             following: 233
+//         });
+//     }, 1000)
+// })
+
+// promiseThree.then((userData) => {
+//     console.log(`userName => ${userData.userName}\nuserpassword is => ${userData.password}`)
+// })
+
+
+
+
+
+
+
+
+// const promiseFour = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         let error = false
+//         if (!error) {
+//             resolve({
+//                 userName: 'ab.19',
+//                 password: '12345678900',
+//                 emailAddress: 'example@gmail.com',
+//                 userAge: 30,
+//                 followers: 659,
+//                 following: 233
+//             })
+//         } else {
+//             reject('ERROR: Something went wrong')
+//         }
+//     }, 1000)
+// })
+// promiseFour.then((userData) => {
+//     return userData.userName;
+// }).then((userName) => {
+//     console.log(`userName => ${userName}`);
+// }).catch((error) => {
+//     console.log(error)
+// }).finally(() => console.log('The promise is either resolve or rejected'))
+
+
+
+
+
+
+
+
+const promiseFive = new Promise((resolve, reject) => {
     setTimeout(() => {
         let error = false
         if (!error) {
             resolve({
-                userName: 'hb07',
-                password: '12345678900',
-                emailAddress: 'example@gmail.com',
-                userAge: 17,
+                userName: 'li.39',
+                password: '0123456789',
+                emailAddress: 'example07@gmail.com',
+                userAge: 19,
                 followers: 659,
                 following: 233
             })
         } else {
-            reject('ERROR: Something went wrong')
+            reject('ERROR: JS went wrong')
         }
     }, 1000)
 })
-promiseFour.then((userData) => {
-    return userData.userName;
-}).then((userName) => {
-    console.log(`userName => ${userName}`);
-}).catch((error) => {
-    console.log(error)
-}).finally(() => console.log('The promise is either resolve or rejected'))
+
+async function consumePromiseFive() {
+    try {
+        const responce = await promiseFive
+        console.log(responce);
+    } catch (error) {
+        console.log('ERROR: error detected in promise five ');
+    }
+}
+
+consumePromiseFive();
