cc50b955a4133a2af58f21b178062ff2a46062c2
Hamzabaloch08 <hb.developer08@gmail.com>
2024-11-09 12:46:59 +0500

    Update app.js

--- OOP/app.js
+++ OOP/app.js
@@ -1,8 +1,30 @@
 // Object literals
-const user = {
+const userData = {
     username: 'hamza',
     loginCount: 8,
-    signedIn: true
+    signedIn: true,
+    getUserDetails: () => {
+        console.log(`Username ${this.username}`);
+        // console.log(this)
+    }
 };
 
-console.log(user);
+// console.log(user.username);
+// console.log(user.getUserDetails());
+
+
+// Constructor function
+function user(username, loginCount, isLoggedIn) {
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn;
+
+    return this;
+};
+
+const userOne = new user('John', 12, true);
+const userTwo = new user('sam', 24, true);
+
+console.log(userOne);
+console.log(userTwo);
+
