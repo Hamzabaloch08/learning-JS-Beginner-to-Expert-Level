f86d7af012663322d33c6f28b0fa2772770bba9b
Hamzabaloch08 <hb.developer08@gmail.com>
2024-11-26 18:37:05 +0500

    Update app.js

--- class-constructor-and-static/app.js
+++ class-constructor-and-static/app.js
@@ -1,4 +1,10 @@
 // es6
+
+
+
+
+
+
 class user {
     constructor(username, email, password) {
         this.username = username;
@@ -20,3 +26,29 @@ const userData = new user('john', 'example@.co', '123');
 console.log(userData);
 console.log(userData.encryptPassword());
 console.log(userData.changeUserName());
+
+
+
+
+// behind the scene 
+
+// function user(username, email, password) {
+//     this.username = username;
+//     this.email = email;
+//     this.password = password;
+// }
+
+// user.prototype.encryptPassword = function () {
+//     return `${this.password}abc`;
+// }
+
+// user.prototype.changeUserName = function () {
+//     return `${this.username.toUpperCase()}`;
+// }
+
+// const userData = new user('john', 'example@.co', '123');
+
+// console.log(userData);
+// console.log(userData.encryptPassword());
+// console.log(userData.changeUserName());
+
