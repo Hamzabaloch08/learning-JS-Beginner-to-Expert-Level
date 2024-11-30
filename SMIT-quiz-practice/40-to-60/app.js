34f6e879bfe730f85c5b32524d5da4c6387c9141
Hamzabaloch08 <hb.developer08@gmail.com>
2024-11-30 22:04:42 +0500

    Create app.js

---
+++ SMIT-quiz-practice/40-to-60/app.js
@@ -0,0 +1,24 @@
+// let color = "red";
+// switch (color) {
+//   case "blue":
+//     console.log("Blue");
+//   case "red":
+//     console.log("Red");
+//   default:
+//     console.log("Default");
+// }
+
+// Red Default (due to missing break)
+
+// let x = 5;
+// while (x > 10) {
+//   console.log(x);
+// }
+
+// 0
+
+// correct syntex of do while loop!
+// do {
+//     statement;
+//   } while (condition);
+
